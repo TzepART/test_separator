a3242eb8b6da615165946ca059b2c36b3979203f
artem.tsepkov <artem.tsepkov@lamoda.ru>
2019-12-18 15:53:40 +0300

    update logic of data converting

--- src/Command/SeparateTestsCommand.php
+++ src/Command/SeparateTestsCommand.php
@@ -8,9 +8,19 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use TestSeparator\Handler\SeparateTestsHandler;
+use TestSeparator\Model\GroupBlockInfo;
 
+/**
+ * Class SeparateTestsCommand
+ * @package TestSeparator\Command
+ */
 class SeparateTestsCommand extends Command
 {
+    /**
+     * @var string
+     */
+    private $groupPrefix = 'time_group_';
+
     /**
      * @var SeparateTestsHandler
      */
@@ -40,15 +50,21 @@ class SeparateTestsCommand extends Command
             ->setHelp('This command allows you to separate tests...');
     }
 
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return int
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         // outputs multiple lines to the console (adding "\n" at the end of each line)
         $output->writeln('Separating started...');
 
-        $suitesFile   = $input->getArgument('allure_file');
+        $suitesFile      = $input->getArgument('allure_file');
         $baseTestDirPath = $input->getArgument('tests_directory');
-        $groupDirPath = $input->getArgument('result_path');
-        $countSuit    = (int) $input->getArgument('count_group');
+        $groupDirPath    = $input->getArgument('result_path');
+        $countSuit       = (int) $input->getArgument('count_group');
 
         $this->separateTests($suitesFile, $baseTestDirPath, $groupDirPath, $countSuit);
 
@@ -57,27 +73,26 @@ class SeparateTestsCommand extends Command
         return 0;
     }
 
+    /**
+     * @param string $suitesFile
+     * @param string $baseTestDirPath
+     * @param string $groupDirPath
+     * @param int    $countSuit
+     */
     private function separateTests(string $suitesFile, string $baseTestDirPath, string $groupDirPath, int $countSuit)
     {
-        $groupPrefix         = 'time_group_';
-        $reFormateResultFile = __DIR__ . '/../../results/result.csv';
-        $timeResultsFile     = __DIR__ . '/../../results/time_results.json';
-        $groupSeparatingFile = __DIR__ . '/../../results/time_results_' . $countSuit . '_sutecases.json';
-
-        $this->separateTestsHandler->reFormateSuitesFile($suitesFile, $reFormateResultFile, $baseTestDirPath);
-        $this->separateTestsHandler->summTimeByDirectories($reFormateResultFile, $timeResultsFile, $baseTestDirPath);
-        $this->separateTestsHandler->separateDirectoriesByTime($timeResultsFile, $groupSeparatingFile, $countSuit);
-
-        $arGroups = json_decode(file_get_contents($groupSeparatingFile), false);
+        $testInfoArray    = $this->separateTestsHandler->reFormateSuitesFile($suitesFile, $baseTestDirPath);
+        $testDirsWithTime = $this->separateTestsHandler->summTimeByDirectories($testInfoArray, $baseTestDirPath);
+        $arGroups         = $this->separateTestsHandler->separateDirectoriesByTime($testDirsWithTime, $countSuit);
 
         // remove all group files
         $this->separateTestsHandler->removeAllGroupFiles($groupDirPath);
 
+        /** @var GroupBlockInfo $arGroupBlockInfo */
         foreach ($arGroups as $groupNumber => $arGroupBlockInfo) {
-            $groupName        = $groupPrefix . $groupNumber;
-            $arGroupBlockInfo = (array) $arGroupBlockInfo;
+            $groupName = $this->groupPrefix . $groupNumber;
 
-            foreach ($arGroupBlockInfo['dir_time'] as $localTestsDir => $time) {
+            foreach ($arGroupBlockInfo->getDirTimes() as $localTestsDir => $time) {
                 file_put_contents($groupDirPath . $groupName . '.txt', 'tests/' . $localTestsDir . PHP_EOL, FILE_APPEND);
             }
         }
