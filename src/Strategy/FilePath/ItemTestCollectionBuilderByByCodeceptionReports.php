460dd33b034bf9cb58f153dd0e01c302baf6ac0f
artem.tsepkov <artem.tsepkov@lamoda.ru>
2020-06-07 17:03:54 +0300

    add logic of ItemTestCollection building by codeception report

--- src/Strategy/FilePath/ItemTestCollectionBuilderByByCodeceptionReports.php
+++ src/Strategy/FilePath/ItemTestCollectionBuilderByByCodeceptionReports.php
@@ -29,6 +29,32 @@ class ItemTestCollectionBuilderByByCodeceptionReports extends AbstractItemTestCo
      */
     public function buildTestInfoCollection(): array
     {
-        // TODO: Implement buildTestInfoCollection() method.
+        $filePaths = $this->getFilePathsByDirectory($this->codeceptionReportDir);
+
+        $results = [];
+        foreach ($filePaths as $filePath) {
+            echo $filePath . PHP_EOL;
+            //TODO add catching if xml is Invalid
+            $xml = simplexml_load_string(file_get_contents($filePath));
+
+            foreach ($xml->{'testsuite'}->children() as $testChild) {
+                $file         = (string) $testChild->attributes()->file;
+                $relativePath = str_replace($this->getBaseTestDirPath(), 'tests/', $file);
+                $dir          = preg_replace('/[A-z0-9]+\.php$/', '', $relativePath);
+                preg_match('/([^ ]+)/', (string) $testChild->attributes()->name, $matches);
+                $test = $matches[1];
+                $time = (int) (((float) $testChild->attributes()->time) * 1000);
+
+                $results[] = new ItemTestInfo(
+                    $dir,
+                    $file,
+                    $relativePath,
+                    $test,
+                    $time
+                );
+            }
+        }
+
+        return $results;
     }
 }
