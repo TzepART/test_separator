76a68b7be52f03af3574bbd249255b59ebd03677
artem.tsepkov <artem.tsepkov@lamoda.ru>
2020-06-09 02:08:00 +0300

    add configuration by yaml file

--- src/Exception/ErrorWhileParsingConfigurationFile.php
+++ src/Exception/ErrorWhileParsingConfigurationFile.php
@@ -5,5 +5,5 @@ namespace TestSeparator\Exception;
 
 class ErrorWhileParsingConfigurationFile extends ValidationOfConfigurationException
 {
-    public const MESSAGE_TEMPLATE = 'Error while parsing configuration file. Error - %s';
+    public const MESSAGE = 'Error while parsing configuration file.';
 }
